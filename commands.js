7b889a6b742b5c7129da45a8c1f1304ca49a3332
小太 <nospam.kotarou.dono@gmail.com>
2012-12-10 23:30:21 +1100

    [WIP] More commands ported to the new system

--- commands.js
+++ commands.js
@@ -30,23 +30,51 @@ function logModCommand(room, str, noBroadcast) {
 function userTargetedPermssion(permission, param) {
 	return function (user, command, params) { return user.can(permission, params[param]); };
 }
-function parseSingleArgument(param) {
+function parseSingleArgument(param, isId) {
 	return function (user, command, args) {
-		if (args.length < 2) return "Not enough arugments";
+		if (args.length < 2) return "Not enough arguments.";
 		var result = {}
 		result[param] = args[1];
+		if (isId) result[param] = toId(result[param]);
+		return result;
+	};
+}
+function parseSingleUserArgument(param, isDefaultToSelf) {
+	return function (user, command, args) {
+		var result = {}
+		if (args.length < 2) {
+			if (!isDefaultToSelf) return "Not enough arguments.";
+			result[param] = user;
+		} else {
+			result[param] = Users.get(args[1]);
+			if (!result[param]) return "User " + args[1] + " not found.";
+		}
+		return result;
+	};
+}
+function parseUserAndReasonArguments(userParam, reasonParam) {
+	return function (user, command, args) {
+		if (args.length < 2) return "Not enough arguments.";
+
+		var result = {}
+		result[userParam] = Users.get(args[1]);
+		if (!result[userParam]) return "User " + args[1] + " not found.";
+		result[reasonParam] = args[0].slice(args[1].length).trim().slice(1).trim();
 		return result;
 	};
 }
 
 var commands = {
+	// General commands
 	'nick': 'trn',
 	'trn': {
+		help: "Change your current nickname.",
+		usage: "<nickname>",
 		parseArguments: function (user, command, args) {
-			if (args.length < 2) return "Not enough arugments";
+			if (args.length < 2) return "Not enough arguments.";
 			var result = {};
 			result.nick = args[1];
-			if (args[2]) result.auth = true;
+			if (parseInt(args[2], 10)) result.auth = true;
 			if (args[3]) {
 				args.splice(0, 3);
 				result.token = args.join(',');
@@ -82,13 +110,105 @@ var commands = {
 			room.add('|c| Birkal|/' + (command === 'birkal' ? 'me' : 'mee') + (args[0] ? ' ' + args[0] : ''));
 		}
 	},
+	'avatar': {
+		help: "Change your trainer sprite.",
+		usage: "<avatar number>",
+		parseArguments: function (user, command, args) {
+			if (args.length < 2) return {};
+			var result = {avatar: parseInt(args[1], 10)};
+			if (!result.avatar || result.avatar < 1 || result.avatar > 294) return "Invalid avatar."
+			return result;
+		},
+		handler: function (user, command, params, output, room, socket) {
+			if (!params.avatar) return "/avatars";
+			user.avatar = params.avatar;
+			output.write("Avatar changed to:");
+			output.writeHtml("<img src=\"/sprites/trainers/" + user.avatar + ".png\" alt=\"\" />");
+		}
+	},
+	'whois': {
+		help: "Get details on a user.",
+		usage: "<nickname>",
+		isBroadcastable: true,
+		parseArguments: parseSingleUserArgument('targetUser'),
+		handler: function (user, command, params, output, room, socket) {
+			output.write("User: " + params.targetUser.name);
+			if (config.groups[params.targetUser.group] && config.groups[params.targetUser.group].name) {
+				output.write("Group: " + config.groups[params.targetUser.group].name + " (" + params.targetUser.group + ")");
+			}
+			if (!params.targetUser.authenticated) output.write("(Unregistered)");
+			if (!params.targetUser.connected) output.write("(Offline)");
+			if (user.can('ip', params.targetUser)) output.write("IP: " + params.targetUser.ip);
+
+			if (!params.targetUser.connected) return;
+			var outputMessage = [];
+			for (var roomId in params.targetUser.roomCount) {
+				outputMessage.push("<a href=\"" + roomId + "\" onclick=\"return selectTab('" + roomId + "');\">" + roomId + "</a>");
+			}
+			if (outputMessage.length === 0) {
+				output.write("In rooms: None (How suspicious!)");
+			} else {
+				output.write("In rooms: " + outputMessage.join(" | "));
+			}
+		}
+	},
+	'rooms': {
+		help: "Show what rooms a user is in.",
+		usage: "<nickname>",
+		isBroadcastable: true,
+		parseArguments: parseSingleUserArgument('targetUser'),
+		handler: function (user, command, params, output, room, socket) {
+			if (!params.targetUser.connected) {
+				output.write(params.targetUser.name + " is offline.");
+				return;
+			}
+			var outputMessage = [];
+			for (var roomId in params.targetUser.roomCount) {
+				outputMessage.push("<a href=\"" + roomId + "\" onclick=\"return selectTab('" + roomId + "');\">" + roomId + "</a>");
+			}
+			if (outputMessage.length === 0) {
+				output.write(params.targetUser.name + " is not in any rooms. (How suspicious!)");
+			} else {
+				output.write(params.targetUser.name + " is in: " + outputMessage.join(" | "));
+			}
+		}
+	},
+	'altcheck': 'alts',
+	'getalts': 'alts',
+	'alts': {
+		help: "Get a user's alts.",
+		usage: "<nickname>",
+		isBroadcastable: true,
+		parseArguments: parseSingleUserArgument('targetUser', true),
+		permission: 'alts',
+		permissionHandler: userTargetedPermssion('alts', 'targetUser'),
+		handler: function (user, command, params, output, room, socket) {
+			var alts = params.targetUser.getAlts();
+			alts.unshift(params.targetUser.name);
+			for (var a = 0; a < alts.length; ++a) {
+				var alt = Users.get(alts[a]);
+				if (!alt.named || !alt.connected || !user.can('alts', alt)) continue;
+				output.write("User: " + alt.name);
+				if (alt.prevNames && Object.keys(alt.prevNames).length > 0) output.write("Previous names: " + Object.keys(alt.prevNames).join(", "));
+			}
+		}
+	},
+
+	// Battle commands
+	'surrender': 'forfeit',
+	'concede': 'forfeit',
+	'forfeit': {
+		isBattleOnly: true,
+		handler: function (user, command, args, output, room, socket) {
+			if (!room.forfeit(user)) output.write("You can't forfeit this battle.");
+		}
+	},
 
 	// Moderator commands
 	'nl': 'namelock',
 	'namelock': {
-		help: "Locks a user's name.\nUsage: /namelock <target user> [, <target name>]",
-		permission: 'namelock',
-		permissionHandler: userTargetedPermssion('namelock', 'targetUser'),
+		help: "Locks a user's name.",
+		usage: "<nickname> [, <target nickname>]",
 		parseArguments: function (user, command, args) {
 			if (args.length < 2) return "Not enough arguments";
 
@@ -100,6 +220,8 @@ var commands = {
 			if (args[2]) result.targetName = args[2];
 			return result;
 		},
+		permission: 'namelock',
+		permissionHandler: userTargetedPermssion('namelock', 'targetUser'),
 		handler: function (user, command, params, output, room, socket) {
 			var oldname = params.targetUser.name;
 			var targetId = toUserid(params.targetName);
@@ -136,21 +258,60 @@ var commands = {
 	'nul': 'nameunlock',
 	'unnamelock': 'nameunlock',
 	'nameunlock': {
-		help: "Unlocks a name-locked user.\nUsage: /nameunlock <target user>",
+		help: "Unlocks a name-locked user.",
+		usage: "<nickname>",
+		parseArguments: parseSingleArgument('targetUser', true),
 		permission: 'namelock',
-		parseArguments: parseSingleArgument('targetUser'),
 		handler: function (user, command, params, output, room, socket) {
-			var isRemoved = false;
+			var isUnlocked = false;
 			for (var ip in nameLockedIps) {
-				if (nameLockedIps[ip] === params.targetUser) {
+				if (toId(nameLockedIps[ip]) === params.targetUser) {
 					delete nameLockedIps[ip];
-					isRemoved = true;
+					isUnlocked = true;
 				}
 			}
-			if (isRemoved) {
+			if (isUnlocked) {
 				logModCommand(room, user.getIdentity() + " unlocked the name of " + params.targetUser + ".");
 			} else {
 				output.write(params.targetUser + " is not name-locked.");
+			}
+		}
+	},
+	'b': 'ban',
+	'ban': {
+		help: "Kick a user from all rooms and bans the user's IP address with reason.",
+		usage: "<nickname> [, <reason>]",
+		parseArguments: parseUserAndReasonArguments('targetUser', 'reason'),
+		permission: 'ban',
+		permissionHandler: userTargetedPermssion('ban', 'targetUser'),
+		handler: function (user, command, params, output, room, socket) {
+			logModCommand(room, user.getIdentity() + " has banned " + params.targetUser.name + "." + (params.reason ? " (" + params.reason + ")" : ""));
+			if (params.targetUser.getAlts().length > 0) {
+				logModCommand(room, "The alts of " + params.targetUser.name + " were also banned: " + params.targetUser.getAlts().join(", "));
+			}
+			params.targetUser.emit('message', user.getIdentity() + " has banned you. If you feel that your banning was unjustified you can "
+			                                  + "<a href=\"http://www.smogon.com/forums/announcement.php?f=126&a=204\" target=\"_blank\">appeal the ban</a>."
+			                                  + (params.reason ? "<br />The reason for your ban was: " + params.reason : ""));
+			params.targetUser.ban();
+		}
+	},
+	'unban': {
+		help: "Unbans a user.",
+		usage: "<nickname>",
+		parseArguments: parseSingleArgument('targetUser', true),
+		permission: 'ban',
+		handler: function (user, command, params, output, room, socket) {
+			var isUnbanned = false;
+			for (var ip in bannedIps) {
+				if (toId(bannedIps[ip]) === params.targetUser) {
+					delete bannedIps[ip];
+					isUnbanned = true;
+				}
+			}
+			if (isUnbanned) {
+				logModCommand(room, user.getIdentity() + " has unbanned " + params.targetUser + ".");
+			} else {
+				output.write(params.targetUser + " is not banned.");
 			}
 		}
 	},
@@ -158,8 +319,9 @@ var commands = {
 	// Informational commands
 	'stats': 'data',
 	'data': {
+		help: "Get details on a pokemon/item/move/ability.",
+		usage: "<pokemon/item/move/ability>",
 		isBroadcastable: true,
-		help: "Get details on a pokemon/item/move/ability.\nUsage: /data <pokemon/item/move/ability>",
 		parseArguments: function (user, command, args) {
 			if (args.length < 2) return "Not enough arguments";
 
@@ -185,17 +347,18 @@ var commands = {
 		}
 	},
 	'help': {
-		isBroadcastable: true,
 		help: "Show help messages for commands.",
+		usage: "[command]",
+		isBroadcastable: true,
 		parseArguments: function (user, command, args) {
 			var result = {};
 			if (command === 'help' && args.length === 0) {
 				result.isOverview = true;
-			} else if (cmd === 'help' && args.length > 1) {
+			} else if (command === 'help' && args.length > 1) {
 				result.cmd = args[1];
 				var calledCmd = args[1];
 				while (typeof commands[calledCmd] === 'string') calledCmd = commands[calledCmd];
-				if (!commands[calledCmd]) return "The command '" + cmd + "' doesn't exist. Try /help for general help";
+				if (!commands[calledCmd]) return "The command '" + result.cmd + "' doesn't exist. Try /help for general help";
 				result.calledCmd = calledCmd;
 			} else {
 				result.cmd = command;
@@ -220,6 +383,7 @@ var commands = {
 			} else {
 				var helpLines = commands[params.calledCmd].help.split('\n');
 				for (var l = 0; l < helpLines.length; ++l) output.write(helpLines[l]);
+				if (commands[params.calledCmd].usage) output.write("Usage: /" + params.cmd + " " + commands[params.calledCmd].usage);
 				if (commands[params.calledCmd].permission) {
 					output.write("The '" + commands[params.calledCmd].permission + "' permission is required to use this command.");
 				}
@@ -264,7 +428,7 @@ function parse(user, command, room, socket) {
 			output = {
 				add: function (str) { room.add(str); },
 				write: function (str) { room.add('||'+str); },
-				writeRaw: function (str) { room.addRaw(str); },
+				writeHtml: function (str) { room.addRaw(str); },
 				eval: function (str) { for (var u in room.users) room.users[u].emit('console', {evalRawMessage: str}); }
 			};
 		}
@@ -272,7 +436,7 @@ function parse(user, command, room, socket) {
 			output = {
 				add: function (str) { sendData(socket, '>' + room.id + '\n' + str); },
 				write: function (str) { emit(socket, 'console', str); },
-				writeRaw: function (str) { emit(socket, 'console', {rawMessage: str}); },
+				writeHtml: function (str) { emit(socket, 'console', {rawMessage: str}); },
 				eval: function (str) { emit(socket, 'console', {evalRawMessage: str}); }
 			};
 		}
@@ -283,6 +447,10 @@ function parse(user, command, room, socket) {
 			// Default case goes here
 			if (isBroadcasting) return true;
 			emit(socket, 'console', "The command '" + cmd + "' was unrecognized. To send a message starting with '/" + cmd + "', type '//" + cmd + "'. Alternatively, try '/help'");
+			return;
+		}
+		if (commands[calledCmd].isBattleOnly && room.type !== 'room') {
+			emit(socket, 'console', "The command '" + cmd + "' may only be used in battle rooms.");
 			return;
 		}
 		if (isBroadcasting) {
@@ -308,6 +476,7 @@ function parse(user, command, room, socket) {
 		if (typeof params !== 'object') {
 			emit(socket, 'console', "Error parsing arguments to '" + cmd + "'" + (typeof params === 'string' ? ":" : "."));
 			if (typeof params === 'string') emit(socket, 'console', params);
+			if (commands[calledCmd].usage) emit(socket, 'console', "Usage: /" + cmd + " " + commands[calledCmd].usage);
 			return;
 		}
 
