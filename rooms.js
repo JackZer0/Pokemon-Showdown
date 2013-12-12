eda84cbebbe02f20cb7f4c9b88cce81216e133a3
小太 <nospam@xiaotai.org>
2013-12-12 20:44:53 +1100

    Add reminders
    Conflicts:
    	config/commands.js
    	rooms.js

--- rooms.js
+++ rooms.js
@@ -1395,6 +1395,8 @@ var ChatRoom = (function() {
 		var userList = this.userList ? this.userList : this.getUserList();
 		var modchat = this.getModchatNote();
 		this.send('|init|chat\n|title|'+this.title+'\n'+userList+'\n'+this.logGetLast(25).join('\n')+modchat, connection);
+		if (this.reminders && this.reminders.length > 0)
+			CommandParser.parse('/reminder view', this, user, connection);
 	};
 	ChatRoom.prototype.onJoin = function(user, connection, merging) {
 		if (!user) return false; // ???
@@ -1418,6 +1420,8 @@ var ChatRoom = (function() {
 			var userList = this.userList ? this.userList : this.getUserList();
 			var modchat = this.getModchatNote();
 			this.send('|init|chat\n|title|'+this.title+'\n'+userList+'\n'+this.logGetLast(100).join('\n')+modchat, connection);
+			if (this.reminders && this.reminders.length > 0)
+				CommandParser.parse('/reminder view', this, user, connection);
 		}
 
 		return user;
